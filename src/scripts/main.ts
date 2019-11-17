37560454e2681d11db9a289c2b015c3159c90002
Ostrovskyi <german.ostrovskij@gmail.com>
2019-11-17 13:53:25 +0100

    Add testimonials slider

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,8 +3,10 @@ import {Navigation} from "./navigation";
 // @ts-ignore
 import  * as SmoothScroll from "../../node_modules/smooth-scroll/dist/smooth-scroll";
 import {ToTop} from "./toTop";
+import {Sideshow} from "./sideshow";
 
 new Navigation('main-nav');
 new SmoothScroll('a[href*="#"]');
 new FormValidator('contacts-form');
 new ToTop();
+new Sideshow('testimonials');
